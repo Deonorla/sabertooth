3e5d9e549af8755d7cdd0989de364235c366e7a1
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-11-13 15:12:25 +0100

    [update]

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    plugins: [react()],
     define: {
       "process.env.MAILCHIMP_API_KEY": JSON.stringify(env.MAILCHIMP_API_KEY),
       "process.env.LIST_ID": JSON.stringify(env.LIST_ID),

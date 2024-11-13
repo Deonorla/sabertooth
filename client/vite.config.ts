cd7d3b8741643e0f4e9aeabe51b05cde394bacd7
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-11-13 14:39:27 +0100

    [changes]

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,9 +8,9 @@ export default defineConfig(({ mode }) => {
       "process.env.MAILCHIMP_API_KEY": JSON.stringify(env.MAILCHIMP_API_KEY),
       "process.env.LIST_ID": JSON.stringify(env.LIST_ID),
     },
-    build: {
-      outDir: "client/dist",
-      assetsDir: "assets",
-    },
+    // build: {
+    //   outDir: "client/dist",
+    //   assetsDir: "assets",
+    // },
   };
 });

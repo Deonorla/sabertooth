3067d83aa24d20b6a891a5ae8bf80cb662efd765
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2024-11-13 13:34:39 +0100

    [updated vercel.json && vite.config.ts]

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,11 +7,9 @@ export default defineConfig(({ mode }) => {
     define: {
       "process.env.MAILCHIMP_API_KEY": JSON.stringify(env.MAILCHIMP_API_KEY),
       "process.env.LIST_ID": JSON.stringify(env.LIST_ID),
-      // If you want to exposes all env variables, which is not recommended
-      // 'process.env': env
     },
     build: {
-      outDir: "dist", // Ensure Vite outputs to "dist"
+      outDir: "client/dist",
     },
   };
 });
